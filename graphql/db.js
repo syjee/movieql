91b74dbe28cbcd2f1f7c37804fbed9f6d84dfa0a
syjee <syjee95@kpu.ac.kr>
2018-10-24 19:07:58 +0900

    add func & delete func

--- graphql/db.js
+++ graphql/db.js
@@ -1,46 +1,75 @@
 //import { findRemovedLocationsForDirective } from "graphql/utilities/findBreakingChanges";
 //import { defaultCreateRemoteResolver } from "graphql-tools";
 
-export const people = [
+let movies = [
     {
-        id : 0,
-        name :  "zionMyung",
-        age :  24,
-        gender :  "female"
+        id: 1,
+        name: "About-Time",
+        genre: "romaticLove",
+        score: 10
     },
     {
-        id :  1,
-        name : "nicolas",
-        age :  18,
-        gender :  "male"
+        id: 2,
+        name: "Notebook",
+        genre: "romaticLove",
+        score: 10
     },
     {
-        id : 2,
-        name :  "soyoungJi",
-        age :  24,
-        gender :  "female"
+        id: 3,
+        name: "Gravity",
+        genre: "SF",
+        score: 10
     },
     {
-        id : 3,
-        name :  "somiJi",
-        age :  26,
-        gender :  "female"
+        id: 4,
+        name: "One-day",
+        genre: "romanticLove",
+        score: 10
     },
     {
-        id : 4,
-        name :  "chusilJeon",
-        age :  54,
-        gender :  "female"
+        id: 5,
+        name: "Les-Miserables",
+        genre: "drama",
+        score: 10
     },
     {
-        id :  5,
-        name :  "changyoungJi",
-        age :  51,
-        gender :  "male"
-    } 
+        id: 6,
+        name: "The Greatest Show",
+        genre: "drama",
+        score: 10
+    },
+    {
+        id: 7,
+        name: "Gladiator",
+        genre: "Action",
+        score: 10
+    }
+
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filterdPeople = people.filter(item => item.id === id);
-    return filterdPeople[0];
+    const filterdMovies = movies.filter(item => item.id === id);
+    return filterdMovies[0];
+}
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length){
+        movies = cleanedMovies;
+        return true;
+    }else return false;
+
+}
+
+export const addMovie = (name, genre, score) =>{
+    const newMovie = {
+        id : `${movies.length + 1}`,
+        name,
+        genre,
+        score
+    }
+    movies.push(newMovie);
+    return newMovie;
 }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,17 @@
-import { people, getById } from "./db";
+import { getById, getMovies, deleteMovie,addMovie } from "./db";
 
 const resolvers = {
     Query : {
-        people : () => people,
-        person : (_, { id }) => getById(id)
+        movies : () => getMovies(),
+        movie : (_, { id }) => getById(id),
+
+    },
+    Mutation: {
+        addMovie: (_, {name, genre,score}) => addMovie(name,genre,score),
+        deleteMovie : (_,{id})=> deleteMovie(id)
     }
 };
+
+
 
 export default resolvers; 
